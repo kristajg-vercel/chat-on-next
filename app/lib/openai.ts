6f1f4b0cb8758d5b1712e811425ab5e4ca7c0953
kristajg-vercel <krista.goralczyk@vercel.com>
2024-03-12 13:47:55 -0600

    rm openai

--- app/lib/openai.ts
+++
@@ -1,8 +0,0 @@
-// import OpenAI from "openai";
-
-// const openaiApiKey = process.env.OPENAI_API_KEY || '';
-// console.log('not a good thing ', openaiApiKey)
-// export const openai = new OpenAI.OpenAI({
-//   apiKey: openaiApiKey,
-//   dangerouslyAllowBrowser: true,
-// });
--- server/openai.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-import OpenAI from "openai";
-
-const openaiApiKey = process.env.OPENAI_API_KEY || '';
-export const openai = new OpenAI.OpenAI({
-  apiKey: openaiApiKey,
-  dangerouslyAllowBrowser: true,
-});
